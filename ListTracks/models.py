77df09aa45b2f285059f52eaa935f77d92c9aa10
hammerton <monkey_man2k@hotmail.com>
2014-06-22 19:36:01 -0400

    Just some cosmetic work

--- ListTracks/models.py
+++ ListTracks/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 from django.utils.encoding import smart_unicode
 
+
 # Create your models here.
 class Track(models.Model):
-  title = models.CharField(max_length=120)
-  artist = models.CharField(max_length=120)
-  scid = models.CharField(max_length=50)
-  scurl = models.CharField(max_length=255)
-  twitter = models.CharField(max_length=50)
-  _id = models.CharField(max_length=50)
-  embedhtml = models.TextField()
-  dateadded = models.DateTimeField(auto_now_add = True, auto_now = False)
-  
-  def __unicode__(self):
+    title = models.CharField(max_length=120)
+    artist = models.CharField(max_length=120)
+    scid = models.CharField(max_length=50)
+    scurl = models.CharField(max_length=255)
+    twitter = models.CharField(max_length=50)
+    _id = models.CharField(max_length=50)
+    embedhtml = models.TextField()
+    dateadded = models.DateTimeField(auto_now_add = True, auto_now = False)
+
+    def __unicode__(self):
         return smart_unicode(self.title + ' - ' + self.artist)
--- ListTracks/views.py
+++ ListTracks/views.py
@@ -1,42 +1,12 @@
-import soundcloud
-import json
-import urllib2
 from django.shortcuts import render
 from ListTracks.AppCode.TrackService import TrackService
 from ListTracks.models import Track
 
+
 # Create your views here.
-
 def home(request):
-  # http://www.pythonforbeginners.com/python-on-the-web/parsingjson/
-  # http://poolsideapi2.herokuapp.com/tracks
-  # http://stackoverflow.com/questions/6578986/how-to-convert-json-data-into-a-python-object
-  # http://stackoverflow.com/questions/4528099/convert-string-to-json-using-python
-  # http://www.siteground.com/tutorials/siteground-git/clone-git-repository.htm
+    t = TrackService()
+    t.insertNewTracks()
 
-#   # create a client object with your app credentials
-#   client = soundcloud.Client(client_id='e72237107739281ffceb867534efd87c')
-  
-#   response = urllib2.urlopen('http://poolsideapi2.herokuapp.com/tracks')
-#   tracks = json.loads(response.read())
-  
-#   for t in tracks[:3]:
-#       try:
-#         embed_info = client.get('/oembed', url=t['scUrl'])
-# #         print embed_info.__getattr__('html')
-#         t['embedHtml'] = embed_info.__getattr__('html')
-#       except:
-#         pass
-      
-#   return render(request, 'index.html', {'tracks': tracks})
-
-  t = TrackService()
-  t.insertNewTracks()
-  
-  tracks = Track.objects.all()[:10]
-  return render(request, 'index.html', {'tracks': tracks})
-
-
-
-
-
+    tracks = Track.objects.all()[:10]
+    return render(request, 'index.html', {'tracks': tracks})
