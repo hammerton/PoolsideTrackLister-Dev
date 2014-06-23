ebec23be8a7036447a5a4488232eb9c237022df0
hammerton <monkey_man2k@hotmail.com>
2014-06-23 00:24:42 -0400

    some small changes

--- PoolsideTrackLister/settings.py
+++ PoolsideTrackLister/settings.py
@@ -81,7 +81,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+if DEBUG:
+    MEDIA_URL = '/media/'
+    STATIC_ROOT = '/static/'
+    MEDIA_ROOT = '/static/media/'
+    STATICFILES_DIRS = (
+        'static',
+    )
 
+#Template location
 TEMPLATE_DIRS = (
   "static/templates",
 )
--- PoolsideTrackLister/urls.py
+++ PoolsideTrackLister/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'PoolsideTrackLister.views.home', name='home'),
@@ -11,3 +14,7 @@ urlpatterns = patterns('',
     url(r'^$', 'ListTracks.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
