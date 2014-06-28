a3a2abd890803de0203e1810d1b1908f638a1673
hammerton <monkey_man2k@hotmail.com>
2014-06-28 02:10:27 -0400

    yea yea

--- PoolsideTrackLister/settings.py
+++ PoolsideTrackLister/settings.py
@@ -91,7 +91,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 if DEBUG:
     MEDIA_URL = '/media/'
-    STATIC_ROOT = 'staticfiles'
+    STATIC_ROOT = '/static/'
     MEDIA_ROOT = '/static/media/'
     STATICFILES_DIRS = (
         'static',
