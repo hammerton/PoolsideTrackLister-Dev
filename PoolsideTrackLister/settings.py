5864dbeb10639ca80a3861064d2983888de981d2
hammerton <monkey_man2k@hotmail.com>
2014-06-28 17:26:36 -0400

    more debug

--- PoolsideTrackLister/settings.py
+++ PoolsideTrackLister/settings.py
@@ -90,7 +90,6 @@ DATABASES = {
 }
 
 if not DEBUG:
-    print "Prod"
     # Parse database configuration from $DATABASE_URL
     import dj_database_url
     DATABASES['default'] = dj_database_url.config()
